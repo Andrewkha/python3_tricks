0e29689b43beea12ce63e0c2584292658deec0df
Chernyshov, Andrey <andrey.chernyshov@intel.com>
2019-09-20 10:40:36 +0300

    iterators finished

--- 15_iterators.py
+++ 15_iterators.py
@@ -1,3 +1,6 @@
+# 2 classes
+
+
 class RepeaterIterator:
     def __init__(self, source):
         self.source = source
@@ -14,6 +17,76 @@ class Repeater:
         return RepeaterIterator(self)
 
 
-repeater = Repeater('Hello')
-for item in repeater:
-    print(item)
+# repeater = Repeater('Hello')
+# for item in repeater:
+#     print(item)
+
+
+#  Single class
+class SingleClassRepeater:
+    def __init__(self, value):
+        self.value = value
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.value
+
+
+# experimenting - returning the letters from the word
+class WordIterator:
+
+    def __init__(self, value):
+        self.value = value
+        self.current = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current < len(self.value):
+            next_char = self.value[self.current]
+            self.current += 1
+            return next_char
+        else:
+            raise StopIteration
+
+
+name = 'Andrey'
+iter_ = WordIterator(name)
+# print(next(iter_))
+# print(next(iter_))
+# print(next(iter_))
+# print(next(iter_))
+# print(next(iter_))
+# print(next(iter_))
+#
+# for char in range(0, len(name)):
+#     print(next(iter_))
+
+for char in iter_:
+    print(char)
+
+
+#  Bounded repeater
+class BoundedRepeater:
+    def __init__(self, value, max_count):
+        self.value = value
+        self.max_count = max_count
+        self.current = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current < self.max_count:
+            self.current += 1
+            return self.value
+        else:
+            raise StopIteration
+
+
+iter2 = BoundedRepeater('Hello', 5)
+for val in iter2:
+    print(val)
